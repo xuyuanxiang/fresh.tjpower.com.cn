6c170399c1b740004a23dcd9812298644140d771
Chaos Xu (zn徐远翔) <15887805082@qq.com>
2014-05-16 09:16:35 +0800

    调整包目录结构

--- js/index.js
+++ js/index.js
@@ -3,11 +3,4 @@ $('.content-top-nav').slick({
 	slidesToShow : 4,
 	slidesToScroll : 4,
 	slide : 'a'
-});
-
-function active(id) {
-	$('.icon').removeClass('active');
-	$('#' + id + 'Ctrl').addClass('active')
-	$('.content-item').addClass('inactive');
-	$('#' + id).removeClass('inactive');
-}
+}); 
